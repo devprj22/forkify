10cd5fbb34a191bd38aff5b31187c536fffda585
prj <shdwmaster632@gmail.com>
2020-07-18 17:35:17 +0530

    Setting up Babel

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,5 @@ import x from './test'
 console.log(x);
 console.log("This text is added on the fly");
 console.log("This will automatically be reloaded");
+
+console.log(`Now this ${x} is printed using ES6 and Babel will convert it to ES5`);
