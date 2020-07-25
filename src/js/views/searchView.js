2ca028918e08f70a44e3bfd5e3802f1f80bcd4b1
prj <shdwmaster632@gmail.com>
2020-07-25 16:59:28 +0530

    Prettifying search result output

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -10,6 +10,29 @@ export const clearResults = () => {
     elements.searchResList.innerHTML = '';
 };
 
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+
+    if (title.length > limit) {
+        
+        title.split(' ').reduce((acc, curr) => {
+        
+            if (acc + curr.length <= limit) {
+                newTitle.push(curr);
+            }
+
+            return acc + curr.length;
+        }, 0);
+
+        // Return the result
+        // Join method is the opposite of split here.
+
+        return `${newTitle.join(' ')} ...`;
+    }
+    
+    return title;
+};
+
 const renderRecipe = recipe => {
     const markup = `
         <li>
@@ -18,7 +41,7 @@ const renderRecipe = recipe => {
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.title}</h4>
+                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
